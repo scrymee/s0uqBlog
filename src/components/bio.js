0b66a3ca1439bc72409f66c29d20ae609d387a41
scrymee <55278680+scrymee@users.noreply.github.com>
2024-10-05 11:32:40 +0900

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,10 +44,11 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
+          Author: <strong>{author.name}</strong> <br />
+          {author?.summary || null}
+          {` `}<br />
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+            Twitter
           </a>
         </p>
       )}
